20da4346f94992e934085ea7d1f7ad5cd2eeb7ee
Muhammad Ammar <ammarkhuram6@gmail.com>
2026-08-04 21:04:58 +0500

    Make API_URL safe with protocol check

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
-const API_URL =
+let rawUrl =
   import.meta.env.VITE_API_URL ||
   "https://hk-korean-institute-production.up.railway.app";
+
+// Yeh ensure karega ke agar http/https na ho toh khud add ho jaye
+const API_URL = rawUrl.startsWith("http") ? rawUrl : `https://${rawUrl}`;
 
 export default API_URL;
